22c2e027c809309ba586c24dc98b35a41e917dfe
chamod udara <uchamod52@gmail.com>
2024-08-24 03:46:31 +0530

    add bottom nav bar and reusable item detail card

---
+++ lib/pages/main_screen.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spread/pages/main_pages/add_items.dart';
+import 'package:spread/pages/main_pages/homepage.dart';
+import 'package:spread/pages/main_pages/items.dart';
+import 'package:spread/pages/main_pages/profilepage.dart';
+import 'package:spread/pages/main_pages/watch_page.dart';
+import 'package:spread/util/constants.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  //screens
+  static const List<Widget> _pages = [
+    HomePage(),
+    WatchPage(),
+    AddItems(),
+    Items(),
+    ProfilePage(),
+  ];
+  //page changer
+  void _onTapIcon(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //add gradient background
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            colors: [backgroundBlue, backgroundPurple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: _pages[_selectedIndex],
+        //add radious
+        bottomNavigationBar: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: verPad),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(32),
+              color: backgroundBlue,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    offset: Offset(2, 4),
+                    blurRadius: 4)
+              ]),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(32),
+            child: BottomNavigationBar(
+                type: BottomNavigationBarType.shifting,
+                backgroundColor: backgroundBlue,
+                currentIndex: _selectedIndex,
+                elevation: 2,
+                selectedItemColor: primaryYellow,
+                unselectedItemColor: secondorywhite,
+                onTap: _onTapIcon,
+                items: [
+                  BottomNavigationBarItem(
+                    backgroundColor: backgroundBlue,
+                    icon: SvgPicture.asset(
+                      "assets/house.svg",
+                      colorFilter: ColorFilter.mode(
+                          _selectedIndex == 0 ? primaryYellow : secondorywhite,
+                          BlendMode.srcIn),
+                      height: 25,
+                      width: 25,
+                      semanticsLabel: 'My SVG Image',
+                    ),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    backgroundColor: backgroundBlue,
+                    icon: SvgPicture.asset(
+                      "assets/tv-minimal-play.svg",
+                      colorFilter: ColorFilter.mode(
+                          _selectedIndex == 1 ? primaryYellow : secondorywhite,
+                          BlendMode.srcIn),
+                      height: 25,
+                      width: 25,
+                      semanticsLabel: 'My SVG Image',
+                    ),
+                    label: 'Watch',
+                  ),
+                  BottomNavigationBarItem(
+                    backgroundColor: backgroundBlue,
+                    icon: SvgPicture.asset(
+                      "assets/diamond-plus.svg",
+                      colorFilter: ColorFilter.mode(
+                          _selectedIndex == 2 ? primaryYellow : secondorywhite,
+                          BlendMode.srcIn),
+                      height: 25,
+                      width: 25,
+                      semanticsLabel: 'My SVG Image',
+                    ),
+                    label: 'Add',
+                  ),
+                  BottomNavigationBarItem(
+                    backgroundColor: backgroundBlue,
+                    icon: SvgPicture.asset(
+                      "assets/file-text.svg",
+                      colorFilter: ColorFilter.mode(
+                          _selectedIndex == 3 ? primaryYellow : secondorywhite,
+                          BlendMode.srcIn),
+                      height: 25,
+                      width: 25,
+                      semanticsLabel: 'My SVG Image',
+                    ),
+                    label: "Check",
+                  ),
+                  BottomNavigationBarItem(
+                    backgroundColor: backgroundBlue,
+                    icon: SvgPicture.asset(
+                      "assets/user-cog.svg",
+                      colorFilter: ColorFilter.mode(
+                          _selectedIndex == 4 ? primaryYellow : secondorywhite,
+                          BlendMode.srcIn),
+                      height: 25,
+                      width: 25,
+                      semanticsLabel: 'My SVG Image',
+                    ),
+                    label: 'Profile',
+                  ),
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/util/texystyles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:spread/util/constants.dart';
+
+class Texystyles {
+  TextStyle title = GoogleFonts.poppins(
+      fontSize: 32, fontWeight: FontWeight.bold, color: secondorywhite);
+  TextStyle subtitle = GoogleFonts.poppins(
+      fontSize: 24, fontWeight: FontWeight.w400, color: secondorywhite);
+  TextStyle body = GoogleFonts.poppins(
+      fontSize: 14, fontWeight: FontWeight.w400, color: secondorywhite);
+  TextStyle label = GoogleFonts.poppins(
+      fontSize: 10, fontWeight: FontWeight.w500, color: secondorywhite);
+}
